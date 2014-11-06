8f0b8741245c83b9b4364675cdb87faa58e6ba16
Mirajo <mmj_chicago@yahoo.com>
2014-11-06 04:37:22 -0500

    My Interactive Map
    My Interactive Map

--- WK1/HTML5 Canvas/1-Starting/js/Clipping2.js
+++ WK1/HTML5 Canvas/1-Starting/js/Clipping2.js
@@ -3,9 +3,51 @@ window.onload = function() {
 	if (theCanvas && theCanvas.getContext) {
 	var ctx = theCanvas.getContext("2d");
 	if (ctx) {
-  			
-		
-     						
-		}
+
+		//create variables x,y, radius and offset
+
+		var x = theCanvas.width/2;
+		var y = theCanvas.height/2;
+		var radius = 75;
+		var offset = 50;
+
+		ctx.save();
+
+		ctx.beginPath();
+		ctx.arc(x, y, radius, 0, 2*Math.PI, false);
+		ctx.clip();
+
+		//Draw a blue circle in the clipping mask
+
+		ctx.beginPath();
+		ctx.arc(x -offset, y-offset, radius, 0, 2*Math.PI, false);
+		ctx.fillStyle = "blue";
+		ctx.fill();
+
+		//Draw a yellow circle in the clipping mask
+
+		ctx.beginPath();
+		ctx.arc(x +offset, y, radius, 0, 2*Math.PI, false);
+		ctx.fillStyle = "yellow";
+		ctx.fill();
+
+		//Draw a red circle in the clipping mask
+
+		ctx.beginPath();
+		ctx.arc(x, y+offset, radius, 0, 2*Math.PI, false);
+		ctx.fillStyle = "red";
+		ctx.fill();
+
+		ctx.restore();
+
+		//Stroke the circle
+		ctx.beginPath();
+		ctx.arc(x, y, radius, 0, 2*Math.PI, false);
+		ctx.lineWidth=10;
+		ctx.strokeStyle= "blue";
+		ctx.stroke();
+
+
+    	}
 	}
-}
+};
